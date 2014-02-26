040e7ec746062e2bc4548c8763442c94776b6baa
Leszek Tarkowski <leszek@czterybity.pl>
2014-02-26 14:52:46 +0100

    accumulate

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -1,15 +1,73 @@
 #include <iostream>
 #include <thread>
+//#include <boost/thread.hpp>
+#include <functional>
+//#include <boost/chrono.hpp>
+#include <chrono>
+#include <string>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
+using namespace boost;
 
-void hello()
+class Hello
 {
-    cout << "Hello World" << endl;
+    string msg_;
+public:
+    Hello(string message) : msg_(message)
+    {
+
+    }
+
+    void operator()()
+    {
+        cout << "Hello World from " << msg_ << endl;
+    }
+};
+
+void hello(string msg)
+{
+    cout << "Hello World from " << msg << endl;
+}
+
+thread make_thread(string msg)
+{
+    //Hello h = Hello();
+    //thread th1{ Hello("Leszek") };
+    //string msg = "Leszek";
+    thread th1(hello, msg);
+    //thread th1( [msg]() { hello(msg); });
+//    int a = 10;
+//    std::function<void(void)> hello = [a](){
+//        this_thread::sleep_for(chrono::seconds(1));
+//        cout << "Hello world, " << a << endl;
+//    };
+//    thread th1{ hello };
+    //boost::this_thread::sleep_for(boost::chrono::seconds(1));
+    return th1;
 }
 
 int main()
 {
-    thread th1(hello);
-    th1.join();
+    cout << thread::hardware_concurrency() << endl;
+    {
+        vector<thread> thg;
+        //thread_group thg;
+        for (int i; i < 4 ; ++i)
+        {
+            string msg = "Thread ";
+            msg += lexical_cast<string>(i);
+            thg.emplace_back(hello, msg);
+//            //thread* th = new thread(hello, msg);
+//            //thg.add_thread(th);
+//            thg.create_thread( bind(hello, msg) );
+        }
+        for ( thread& th : thg)
+        {
+            th.join();
+        }
+//        thg.join_all();
+    }
+    //th2.join();
+    //this_thread::sleep_for(chrono::seconds(2));
 }
