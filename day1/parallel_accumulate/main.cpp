82e7a708580d866a9ee526b7494c2b37770019e6
Leszek Tarkowski <leszek@czterybity.pl>
2014-06-09 15:04:13 +0200

    parallel accumulate

--- day1/parallel_accumulate/main.cpp
+++ day1/parallel_accumulate/main.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <thread>
 
 using namespace std;
+
+//template<typename It, typename T>
+//void accumulate_by_ref(It begin, It end, T& result)
+//{
+//    result = accumulate(begin, end, T());
+//}
 
 template<typename It, typename T>
 T parallel_accumulate(It begin, It end, T init)
 {
-    return init;
+    int N = thread::hardware_concurrency();
+    vector<thread> threads;
+    vector<T> results(N);
+    size_t block_size = distance(begin, end)/N;
+
+    It block_start = begin;
+    for(int i = 0 ; i < N ; ++i)
+    {
+        It block_end = block_start;
+        advance(block_end, block_size);
+        if (i == N-1) block_end = end;
+//        threads.emplace_back(&accumulate_by_ref<It, T>,
+//                             block_start, block_end,
+//                             ref(results[i]));
+        threads.emplace_back(
+                 [block_start, block_end, &results, i] ()
+                    { results[i] =
+                      accumulate(block_start, block_end, T() );}
+                            );
+        block_start = block_end;
+    }
+
+    for (thread& th : threads) th.join();
+
+    return accumulate(results.begin(), results.end(), init);
 }
 
 int main()
@@ -19,9 +50,9 @@ int main()
     {
         vec.push_back(i);
     }
-
-    cout << "Just the Sum = " << accumulate(vec.begin(), vec.end(), 0L) << endl;
-    cout << "Parallel Sum = " << parallel_accumulate(vec.begin(), vec.end(), 0L) << endl;
+    cout << "N of cores " << thread::hardware_concurrency() << endl;
+    cout << "Just the Sum = " << accumulate(vec.begin(), vec.end(), 0UL) << endl;
+    cout << "Parallel Sum = " << parallel_accumulate(vec.begin(), vec.end(), 0UL) << endl;
 
     return 0;
 }
